ddb1881248b2125163a15d0507f013f3b497b11c
Erika Alban <erikalban12@gmail.com>
2024-12-04 17:10:01 -0500

    refactors hw3 js

--- w3/hw/script.js
+++ w3/hw/script.js
@@ -23,55 +23,19 @@ let weather = {
 
 // write your code here
 
-let parisTemp = Math.round(weather.paris.temp);
-let parisHum = Math.round(weather.paris.humidity);
+let city = prompt("Enter a city");
+city = city.toLocaleLowerCase().trim().replace(/\s+/g, "");
 
-let tokyoTemp = Math.round(weather.tokyo.temp);
-let tokyoHum = Math.round(weather.tokyo.humidity);
+if (weather[city] !== undefined) {
+  let temperature = weather[city].temp;
+  let celsiusTemp = Math.round(temperature);
+  let fahrenheitTemp = Math.round((temperature * 9) / 5 + 32);
 
-let lisbonTemp = Math.round(weather.lisbon.temp);
-let lisbonHum = Math.round(weather.lisbon.humidity);
-
-let sanfranciscoTemp = Math.round(weather.sanfrancisco.temp);
-let sanfranciscoHum = Math.round(weather.sanfrancisco.humidity);
-
-let osloTemp = Math.round(weather.oslo.temp);
-let osloHum = Math.round(weather.oslo.humidity);
-
-function cityWeather(city) {
-  // a for loop that iterates through the weather objects
-  // if the city object exists, alert with relevant info
-}
-
-let askCity = prompt("Enter a city");
-askCity = askCity.toLocaleLowerCase().replace(/\s+/g, "");
-if (askCity === "paris") {
   alert(
-    `It is currently ${parisTemp}°C in Paris with a humidity of ${parisHum}%`
+    `It is currently ${celsiusTemp}°C (${fahrenheitTemp}°F) in ${city} with a humidity of 80%.`
   );
-}
-if (askCity === "tokyo") {
+} else {
   alert(
-    `It is currently ${tokyoTemp}°C in Paris with a humidity of ${tokyoHum}%`
-  );
-}
-if (askCity === "lisbon") {
-  alert(
-    `It is currently ${lisbonTemp}°C in Paris with a humidity of ${lisbonHum}%`
-  );
-}
-if (askCity === "sanfrancisco") {
-  alert(
-    `It is currently ${sanfranciscoTemp}°C in Paris with a humidity of ${sanfranciscoHum}%`
-  );
-}
-if (askCity === "oslo") {
-  alert(
-    `It is currently ${osloTemp}°C in Paris with a humidity of ${osloHum}%`
-  );
-}
-if (askCity !== "oslo" || "sanfrancisco" || "lisbon" || "tokyo" || "paris") {
-  alert(
-    "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney"
+    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney`
   );
 }
